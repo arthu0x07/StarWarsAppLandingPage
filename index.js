aa62edfad8caaffd49ebf238702993a68440689d
Arthur Santos <arthur_bsggg@hotmail.com>
2021-11-24 06:37:02 -0300

    Header e Main Desktop Finalizados

---
+++ index.js
@@ -0,0 +1,12 @@
+// assets/main/CardVermelhoFundo.png
+// assets/main/CardVermelhoTexto.png
+
+const redcard = document.querySelector(".main-cardVermelhoNotext");
+
+redcard.addEventListener('mouseenter', () => {
+    redcard.setAttribute('src', 'assets/main/CardVermelhoTexto.png')
+});
+
+redcard.addEventListener('mouseleave', () => {
+    redcard.setAttribute('src', 'assets/main/CardVermelhoFundo.png')
+});
