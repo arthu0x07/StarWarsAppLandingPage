dfa8bb9dece4cdc03dec7eff190eac37c0b15c4c
Arthur Santos <arthur_bsggg@hotmail.com>
2021-12-05 13:12:07 -0300

    HOTFIX: Remoção de um ID não utilizado && correção da fonte para telas maiores do texto da seção download...

--- index.js
+++
@@ -1,9 +0,0 @@
-const redcard = document.querySelector(".main-cardVermelhoNotext");
-
-redcard.addEventListener('mouseenter', () => {
-    redcard.setAttribute('src', 'assets/main/CardVermelhoTexto.png')
-});
-
-redcard.addEventListener('mouseleave', () => {
-    redcard.setAttribute('src', 'assets/main/CardVermelhoFundo.png')
-});
